cbde5c217941b5b3be36b59575ae0698d24031c0
ifatBR <ifat.3d@gmail.com>
2026-04-22 20:50:51 +0300

    initial setting

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import Fastify from "fastify";
+import multipart from "@fastify/multipart";
+import cors from "@fastify/cors";
+
+const app = Fastify({
+  logger: true,
+  routerOptions: { ignoreTrailingSlash: true },
+});
+
+app.register(multipart);
+app.register(cors, { origin: ["http://localhost:5173"] });
+app.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
